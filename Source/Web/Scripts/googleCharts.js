8ed1d2e4a221a5a77574901645203d4038abf3ce
Einar Ingebrigtsen <einar@dolittle.com>
2015-05-21 09:28:57 +0200

    Looking up..

--- Source/Web/Scripts/googleCharts.js
+++ Source/Web/Scripts/googleCharts.js
@@ -2,12 +2,14 @@
 
 ko.bindingHandlers.lineChart = {
     init: function (element, valueAccessor, allValueAccessors, viewModel, bindingContext) {
+    },
+    update: function (element, valueAccessor, allValueAccessors, viewModel, bindingContext) {
         optionsInput = valueAccessor();
 
         var options = {
             title: optionsInput.title,
-            width: optionsInput.width || 300,
-            height: optionsInput.height || 300,
+            width: optionsInput.width,
+            height: optionsInput.height,
             backgroundColor: 'transparent',
             animation: {
                 duration: 1000,
@@ -22,44 +24,6 @@ ko.bindingHandlers.lineChart = {
         };
 
         var chart = new google.visualization.LineChart(element);
-
-        /*
-        var dataHash = {};
-
-        
-        var data = new google.visualization.DataTable();
-
-        options.lines.forEach(function (line) {
-            data.addColumn(line.x.type, line.x.name);
-            data.addColumn(line.y.type, line.y.name);
-        });
-
-        function addRow(row, rowIndex) {
-            var value = row[1];
-            if (ko.isObservable(value)) {
-                value.subscribe(function (newValue) {
-                    data.setValue(rowIndex, 1, newValue);
-                    chart.draw(data, options);
-                });
-            }
-
-            var actualValue = ko.unwrap(value);
-            data.addRow([row[0], actualValue]);
-
-            dataHash[row[0]] = actualValue;
-        };
-
-        optionsInput.data().forEach(addRow);
-
-        optionsInput.data.subscribe(function (newValue) {
-            newValue.forEach(function (row, rowIndex) {
-                if (!dataHash.hasOwnProperty(row[0])) {
-                    addRow(row, rowIndex);
-                }
-            });
-
-            chart.draw(data, options);
-        });*/
 
         var array = [];
         var names = [];
@@ -79,54 +43,10 @@ ko.bindingHandlers.lineChart = {
             }
             array.push(row);
         });
-        
-        var data = google.visualization.arrayToDataTable(array);
-        /*
-        var data = google.visualization.arrayToDataTable([
-          ['Year', 'Sales', 'Expenses'],
-          ['2004', 1000, 400],
-          ['2005', 1170, 460],
-          ['2006', 660, 1120],
-          ['2007', 1030, 540]
-        ]);
-        */
 
-        chart.draw(data, options);
-    },
-    update: function (element, valueAccessor, allValueAccessors, viewModel, bindingContext) {
-    }
-};
-
-
-//google.setOnLoadCallback(init);
-
-ko.bindingHandlers.columnChart = {
-    init: function (element, valueAccessor, allValueAccessors, viewModel, bindingContext) {
-        optionsInput = valueAccessor();
-
-        var options = {
-            title: optionsInput.title,
-            hAxis: { title: 'Year', titleTextStyle: { color: 'red' } },
-            width: optionsInput.width || 300,
-            height: optionsInput.height || 300,
-            backgroundColor: 'transparent',
-            animation: {
-                duration: 1000,
-                easing: 'out'
-            }
-        };
-
-        var data = google.visualization.arrayToDataTable([
-            ['Year', 'Sales', 'Expenses', 'Horse'],
-            ['2004', 1000, 400, 2],
-            ['2005', 1170, 460, 300],
-            ['2006', 660, 1120, 100],
-            ['2007', 1030, 540, 500]
-        ]);
-
-        var chart = new google.visualization.ColumnChart(element);
-        chart.draw(data, options);
-    },
-    update: function () {
+        if (Bifrost.isArray(inputData) && inputData.length > 0) {
+            var data = google.visualization.arrayToDataTable(array);
+            chart.draw(data, options);
+        }
     }
 };
